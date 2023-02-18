8147b2322031e686718ab02d68d4e9fab468eb82
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2023-02-18 03:16:10 +0300

    1.1.0 | Add support for mathematical operations on Unit and TokenAmount instances

--- test.py
+++ test.py
@@ -13,39 +13,39 @@ class Account:
     @staticmethod
     def generate_wallets(n: int = 2):
         """Generate wallets."""
+        print('\n--- generate_wallets ---')
         for i in range(n):
             client = Client()
             print(f'{client.account.address} {client.account.key.hex()}')
 
-        print('----------------------------------------------------------------------------')
-
     @staticmethod
     def show_coin_balance():
         """Show balance in coin."""
+        print('\n--- show_coin_balance ---')
         client = Client(private_key=private_key)
         address = '0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b'
         coin_symbol = get_coin_symbol(chain_id=client.network.chain_id)
         print(f'''Your ({client.account.address}) balance: {client.wallet.balance().Ether} {coin_symbol}
-{address} balance: {client.wallet.balance(address=address).Ether} {coin_symbol}
-----------------------------------------------------------------------------''')
+{address} balance: {client.wallet.balance(address=address).Ether} {coin_symbol}''')
 
     @staticmethod
     def show_token_balance():
         """Show balance in token."""
+        print('\n--- show_token_balance ---')
         client = Client(private_key=private_key)
         address = '0xe4C004883BE7EB6e059005aAf6A34B45722fE3c9'
         contract = client.contracts.get(contract_address='0x326c977e6efc84e512bb9c30f76e30c160ed06fb')
         coin_symbol = contract.functions.symbol().call()
         print(f'''Your ({client.account.address}) balance: {client.wallet.balance(token=contract).Ether} {coin_symbol}
 {address} balance: {client.wallet.balance(token='0x326c977e6efc84e512bb9c30f76e30c160ed06fb',
-                                          address=address).Ether} {coin_symbol}
-----------------------------------------------------------------------------''')
+                                          address=address).Ether} {coin_symbol}''')
 
 
 class Contracts:
     @staticmethod
     def get_abi():
         """Get a contract ABI."""
+        print('\n--- get_abi ---')
         network = Networks.Ethereum
         client = Client(private_key='', network=network)
         print(f'''Python list (parsed from Etherscan):
@@ -55,12 +55,12 @@ class Contracts:
         network.api.key = ''
         client = Client(private_key='', network=network)
         print(f'''JSON serialize string (parsed from contract code):
-{client.contracts.get_abi(contract_address='0xE592427A0AEce92De3Edee1F18E0157C05861564', raw_json=True)}
-----------------------------------------------------------------------------''')
+{client.contracts.get_abi(contract_address='0xE592427A0AEce92De3Edee1F18E0157C05861564', raw_json=True)}''')
 
     @staticmethod
     def get_contracts():
         """Get contract instances and print info about tokens."""
+        print('\n--- get_contracts ---')
         tokens = (
             ('BNB', Networks.Ethereum, '0xB8c77482e45F1F44dE1745F52C74426C631bDD52'),
             ('ETH', Networks.BSC, '0x2170ed0880ac9a755fd29b2688956bd959f933f8'),
@@ -79,13 +79,12 @@ Functions ({len(functions)}):''')
 
             print('---')
 
-        print('----------------------------------------------------------------------------')
-
 
 class NFTs:
     @staticmethod
     def get_info():
         """Get info about NFTs."""
+        print('\n--- get_info ---')
         client = Client(private_key='', network=Networks.Ethereum)
         contract_addresses = (
             '0x12b180b635dd9f07a78736fb4e43438fcdb41555', '0x33c6eec1723b12c46732f7ab41398de45641fa42',
@@ -102,20 +101,19 @@ class NFTs:
 
             print('---')
 
-        print('----------------------------------------------------------------------------')
-
 
 class Transactions:
     @staticmethod
     def current_gas_price():
         """Get the current gas price."""
+        print('\n--- current_gas_price ---')
         client = Client(private_key='', network=Networks.Ethereum)
-        print(f'''{client.transactions.current_gas_price(w3=client.w3).GWei} GWei
-----------------------------------------------------------------------------''')
+        print(f'''{client.transactions.current_gas_price(w3=client.w3).GWei} GWei''')
 
     @staticmethod
     def estimate_gas():
         """Get the estimate gas limit for a transaction with specified parameters."""
+        print('\n--- estimate_gas ---')
         client = Client(private_key=private_key)
         tx_params = {
             'nonce': 100,
@@ -123,12 +121,12 @@ class Transactions:
             'to': client.account.address,
             'value': 1000000
         }
-        print(f'''{client.transactions.estimate_gas(w3=client.w3, tx_params=tx_params).Wei} Wei
-----------------------------------------------------------------------------''')
+        print(f'''{client.transactions.estimate_gas(w3=client.w3, tx_params=tx_params).Wei} Wei''')
 
     @staticmethod
     def auto_add_params():
         """Add 'chainId', 'from', 'gasPrice' and 'gas' parameters to transaction parameters if they are missing."""
+        print('\n--- auto_add_params ---')
         client = Client(private_key=private_key)
         tx_params = {
             'nonce': 100,
@@ -139,12 +137,12 @@ class Transactions:
 {tx_params}
 
 Processed transaction params:
-{client.transactions.auto_add_params(tx_params=tx_params)}
-----------------------------------------------------------------------------''')
+{client.transactions.auto_add_params(tx_params=tx_params)}''')
 
     @staticmethod
     def parse_params():
         """Parse params of the existing transaction."""
+        print('\n--- parse_params ---')
         client = Client(private_key='', network=Networks.Ethereum)
         tx = Tx(tx_hash='0xd41a90a7c465a33e028cfc43e3b024c06d8f023ae4a42557269bad93e73edb6c')
         print(f'''Tx instance attribute:
@@ -154,12 +152,12 @@ Tx instance function:
 {tx.parse_params(client=client)}
 
 Tx instance attribute:
-{tx.params}
-----------------------------------------------------------------------------''')
+{tx.params}''')
 
     @staticmethod
     def decode_input_data():
         """Decode input data using contract ABI."""
+        print('\n--- decode_input_data ---')
         client = Client(private_key='', network=Networks.Ethereum)
         contract = client.contracts.get(contract_address='0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f')
         tx_hash = '0xd41a90a7c465a33e028cfc43e3b024c06d8f023ae4a42557269bad93e73edb6c'
@@ -171,12 +169,12 @@ Tx instance attributes:
 {tx.function_identifier} {tx.input_data}
 
 Transactions class function:
-{client.transactions.decode_input_data(client=client, contract=contract, tx_hash=tx_hash)}
-----------------------------------------------------------------------------''')
+{client.transactions.decode_input_data(client=client, contract=contract, tx_hash=tx_hash)}''')
 
     @staticmethod
     def history():
         """Show the transaction history."""
+        print('\n--- history ---')
         client = Client(private_key=private_key, network=Networks.Ethereum)
         address = '0x7E695e0f3BD9ba81EddAA800D06267d32E4CE484'
         history = client.transactions.history(address=address, raw=True)
@@ -215,11 +213,10 @@ ERC-721: {len(history.erc721.all)}
             for tx_hash, tx in history.erc721.all.items():
                 print(f'\t{tx}\n')
 
-        print('----------------------------------------------------------------------------')
-
     @staticmethod
     def find_txs():
         """Find all transactions of interaction with the contract, in addition, you can filter transactions by the name of the contract function."""
+        print('\n--- find_txs ---')
         client = Client(private_key='', network=Networks.Ethereum)
         txs = client.transactions.find_txs(contract='0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f',
                                            address='0x89D022F95dC5073B7479699044fDf6E3Bc74D3b3')
@@ -235,22 +232,21 @@ ERC-721: {len(history.erc721.all)}
         for tx_hash, tx in txs.items():
             print(f'\t{tx}\n')
 
-        print('----------------------------------------------------------------------------')
-
     @staticmethod
     def approved_amount():
         """Get approved amount of token."""
+        print('\n--- approved_amount ---')
         client = Client(private_key='', network=Networks.Ethereum)
         token = client.contracts.get(contract_address='0xf4d2888d29D722226FafA5d9B24F9164c092421E')
         spender = '0xe592427a0aece92de3edee1f18e0157c05861564'
         owner = '0x6a32729ae1a19f0cafd468d0082a1a9355167692'
         approved_amount = client.transactions.approved_amount(token=token, spender=spender, owner=owner)
-        print(f'''{owner} allowed {spender} to spend {approved_amount.Ether} {token.functions.symbol().call()}
-----------------------------------------------------------------------------''')
+        print(f'''{owner} allowed {spender} to spend {approved_amount.Ether} {token.functions.symbol().call()}''')
 
     @staticmethod
     def send_coin():
         """Send transaction in the coin."""
+        print('\n--- send_coin ---')
         client = Client(private_key=private_key)
         print(f'''Balance before sending: {client.wallet.balance().Ether}
 Raw tx:
@@ -261,12 +257,12 @@ Raw tx:
 {tx}
 Receipt:
 {dict(receipt)}
-Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}
-----------------------------------------------------------------------------''')
+Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}''')
 
     @staticmethod
     def send_token():
         """Send transaction in token."""
+        print('\n--- send_token ---')
         client = Client(private_key=private_key)
         for contract, amount in ((client.contracts.get('0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844'), 10.5),
                                  ('0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844', 9999999999)):
@@ -287,11 +283,10 @@ Receipt:
 ---
 ''')
 
-        print('----------------------------------------------------------------------------')
-
     @staticmethod
     def cancel_coin():
         """Cancel a transaction in coin."""
+        print('\n--- cancel_coin ---')
         client = Client(private_key=private_key)
         tx = client.transactions.send(token='', recipient=client.account.address, amount=0.05)
         print(f'Stuck tx:\n{tx}')
@@ -302,12 +297,12 @@ Receipt:
 {tx}
 Receipt:
 {dict(receipt)}
-Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}
-----------------------------------------------------------------------------''')
+Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}''')
 
     @staticmethod
     def cancel_token():
         """Cancel a transaction in token."""
+        print('\n--- cancel_token ---')
         client = Client(private_key=private_key)
         token = '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844'
         tx = client.transactions.send(token=token, recipient=client.account.address)
@@ -319,12 +314,12 @@ Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id
 {tx}
 Receipt:
 {dict(receipt)}
-Balance after sending: {client.wallet.balance(token=token).Ether}
-----------------------------------------------------------------------------''')
+Balance after sending: {client.wallet.balance(token=token).Ether}''')
 
     @staticmethod
     def speed_up_coin():
         """Speed up a transaction in coin."""
+        print('\n--- speed_up_coin ---')
         client = Client(private_key=private_key)
         tx = client.transactions.send(token='', recipient=client.account.address, amount=0.05)
         print(f'Stuck tx:\n{tx}')
@@ -335,12 +330,12 @@ Balance after sending: {client.wallet.balance(token=token).Ether}
 {tx}
 Receipt:
 {dict(receipt)}
-Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}
-----------------------------------------------------------------------------''')
+Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id=client.network.chain_id)}''')
 
     @staticmethod
     def speed_up_token():
         """Speed up a transaction in token."""
+        print('\n--- speed_up_token ---')
         client = Client(private_key=private_key)
         token = '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844'
         tx = client.transactions.send(token=token, recipient=client.account.address)
@@ -352,12 +347,12 @@ Balance after sending: {client.wallet.balance().Ether} {get_coin_symbol(chain_id
 {tx}
 Receipt:
 {dict(receipt)}
-Balance after sending: {client.wallet.balance(token=token).Ether}
-----------------------------------------------------------------------------''')
+Balance after sending: {client.wallet.balance(token=token).Ether}''')
 
     @staticmethod
     def approve():
         """Approve token for swap."""
+        print('\n--- approve ---')
         client = Client(private_key=private_key)
         tx = client.transactions.approve(token='0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
                                          spender='0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45')
@@ -365,21 +360,21 @@ Balance after sending: {client.wallet.balance(token=token).Ether}
         print(f'''Tx:
 {tx}
 Receipt:
-{dict(receipt)}
-----------------------------------------------------------------------------''')
+{dict(receipt)}''')
 
 
 class Miscellaneous:
     @staticmethod
     def custom_network():
         """Initialize the custom network."""
+        print('\n--- custom_network ---')
         network = Network(name='moonriver', rpc='https://rpc.api.moonriver.moonbeam.network')
-        print(f'''{network}
-----------------------------------------------------------------------------''')
+        print(network)
 
     @staticmethod
     def tx_args():
-        """Initialize the custom network."""
+        """Use this to give human names to arguments and then get a tuple or list of arguments to send to transactions."""
+        print('\n--- tx_args ---')
         client = Client(private_key=private_key)
         args = TxArgs(
             amount=1000000,
@@ -391,26 +386,134 @@ class Miscellaneous:
         )
         print(f'''{args}
 {args.list()}
-{args.tuple()}
-----------------------------------------------------------------------------''')
+{args.tuple()}''')
 
     @staticmethod
     def units():
-        """Approve token for swap."""
-        print(f'''Usage for coin: {Ether(0.015200945)}
-Usage for token: {TokenAmount(amount=500.73467)}
-Usage for gas price: {GWei(14.64734)}
-----------------------------------------------------------------------------''')
+        """Use these units when working with the library."""
+        print('\n--- units ---')
+        print(f'''Usage for coin:
+{Ether(0.015200945)}
+
+Usage for token:
+{TokenAmount(amount=500.73467)}
+
+Usage for gas price:
+{GWei(14.64734)}''')
+
+    @staticmethod
+    def unit_math_operations():
+        """Example of mathematical operations on a Unit instance."""
+        print('\n--- unit_math_operations ---')
+        print(f'''Addition
+100 + 20 + 5 = {(Wei(100) + 20 + Wei(5)).Wei}
+4.1 + 1 + 2.0 = {(4.1 + GWei(1) + 2.0).GWei}
+3020 + 100 + 8 = {(Wei(3020) + Wei(100) + 8).Wei}
+0.5 + 0.1 + 0.03 = {(0.5 + 0.1 + Ether(0.03)).Ether}
+
+Subtraction
+100 - 20 - 5 = {(Wei(100) - 20 - 5).Wei}
+4.1 - 1 - 2.0 = {(4.1 - GWei(1) - 2.0).GWei}
+3020 - 100 - 8 = {(Wei(3020) - Wei(100) - 8).Wei}
+0.5 - 0.1 - 0.03 = {(0.5 - 0.1 - Ether(0.03)).Ether}
+
+Multiplication
+100 * 20 * 5 = {(Wei(100) * 20 * 5).Wei}
+4.1 * 1 * 2.0 = {(4.1 * GWei(1) * 2.0).GWei}
+3020 * 100 * 8 = {(Wei(3020) * Wei(100) * 8).Wei}
+0.5 * 0.1 * 0.03 = {(0.5 * 0.1 * Ether(0.03)).Ether}
+
+Division
+100 / 20 / 5 = {(Wei(100) / 20 / 5).Wei}
+4.1 / 1 / 2.0 = {(4.1 / GWei(1) / 2.0).GWei}
+3020 / 100 / 8 = {(Wei(3020) / Wei(100) / 8).Wei}
+0.5 / 0.1 / 0.03 = {(0.5 / 0.1 / Ether(0.03)).Ether}
+
+Less than
+100 < 20: {Wei(100) < 20}
+1 < 2.0 < 4.1: {GWei(1) < 2.0 < 4.1}
+100 < 8 < 3020: {Wei(100) < 8 < Wei(3020)}
+0.03 < 0.5: {0.03 < Ether(0.5)}
+
+Less than or equal to
+100 <= 100: {Wei(100) <= 100}
+4.1 <= 1: {4.1 <= GWei(1)}
+100 <= 3020: {Wei(100) <= Wei(3020)}
+0.5 <= 0.03: {0.5 <= Ether(0.03)}
+
+Equal
+100 == 20: {Wei(100) == 20}
+4.1 == 1: {4.1 == GWei(1)}
+3020 == 3020: {3020 == Wei(3020)}
+0.5 == 0.5: {0.5 == Ether(0.5)}
+
+Not equal to
+100 != 20: {Wei(100) != 20}
+4.1 != 4.1: {4.1 != GWei(4.1)}
+3020 != 3020: {3020 != Wei(3020)}
+0.03 != 0.5: {Ether(0.03) != 0.5}
+
+Great than
+100 > 20: {Wei(100) > 20}
+4.1 > 4.1: {4.1 > GWei(4.1)}
+8 > 3020: {8 > Wei(3020)}
+0.5 > 0.03: {0.5 > Ether(0.03)}
+
+Great and equal
+100 >= 100: {Wei(100) >= 100}
+2.0 >= 4.1: {2.0 >= GWei(4.1)}
+8 >= 3020: {8 >= Wei(3020)}
+0.5 >= 0.03: {0.5 >= Ether(0.03)}''')
+
+    @staticmethod
+    def token_amount_math_operations():
+        """Example of mathematical operations on a TokenAmount instance."""
+        print('\n--- token_amount_math_operations ---')
+        print(f'''Addition
+100 + 20.0 + 5 = {(TokenAmount(100, decimals=6) + 20.0 + TokenAmount(5, decimals=6)).Ether}
+
+Subtraction
+100 - 20.0 - 5 = {(TokenAmount(100, decimals=6) - 20.0 - TokenAmount(5, decimals=6)).Ether}
+
+Multiplication
+100 * 20.0 * 5 = {(TokenAmount(100, decimals=6) * 20.0 * TokenAmount(5, decimals=6)).Ether}
+
+Division
+100 / 20.0 / 5 = {(TokenAmount(100, decimals=6) / 20.0 / TokenAmount(5, decimals=6)).Ether}
+
+Less than
+100 < 20.0: {TokenAmount(100, decimals=6) < 20.0}
+1 < 2.0 < 4.1: {TokenAmount(1, decimals=6) < 2.0 < TokenAmount(4.1, decimals=6) < 20.0}
+
+Less than or equal to
+100 <= 100.0: {TokenAmount(100, decimals=6) <= 100.0}
+4.1 <= 1: {4.1 <= TokenAmount(1, decimals=6)}
+
+Equal
+20.0 == 100: {20.0 == TokenAmount(100, decimals=6)}
+4.1 == 4.1: {TokenAmount(4.1, decimals=6) == 4.1}
+
+Not equal to
+100 != 20: {TokenAmount(100, decimals=6) != 20.0}
+4.1 != 4.1: {4.1 != TokenAmount(4.1, decimals=6)}
+
+Great than
+100 > 20: {100.0 > TokenAmount(20, decimals=6)}
+4.1 > 4.1: {TokenAmount(4.1, decimals=6) > 4.1}
+
+Great and equal
+100 >= 100: {TokenAmount(100, decimals=6) >= 100.0}
+2.0 >= 4.1: {2.0 >= TokenAmount(4.1, decimals=6)}''')
 
     @staticmethod
     def change_decimals():
         """Leave the Ether amount and change the Wei based on the new decimals."""
+        print('\n--- change_decimals ---')
         token = TokenAmount(amount=500.73467)
         print(f'''Instance from Ether amount: {token}
 Change decimals and get Wei: {token.change_decimals(new_decimals=6)}
 Instance with changed decimals: {token}
-Instance from Wei amount: {TokenAmount(amount=500734670000000008192, wei=True)}
-----------------------------------------------------------------------------''')
+Instance from Wei amount: {TokenAmount(amount=500734670000000008192, wei=True)}''')
 
 
 def main() -> None:
@@ -452,6 +555,8 @@ def main() -> None:
     miscellaneous.custom_network()
     miscellaneous.tx_args()
     miscellaneous.units()
+    miscellaneous.unit_math_operations()
+    miscellaneous.token_amount_math_operations()
     miscellaneous.change_decimals()
 
 
